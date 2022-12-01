1fec1e1a2ee8ae267383649e5abbc52a06200eb8
RomainP01 <romainpani01@gmail.com>
2022-12-01 15:23:03 +0100

    "[NDLI2K22] -git ignore config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   token:
-    "MTA0Nzg3MjIwNTIzODg0MTQwNQ.GjsgdZ.d7-3pbzO-JcUwueP46MURyQD_gzEhJkQr_oUJg",
+    "MTA0Nzg3MjIwNTIzODg0MTQwNQ.G6dn3d.6thqJLFqEkbt1cd0ng6drsprwLJ3a9Dl4oz2CE",
 };
